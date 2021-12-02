64ab2d9a143d6b9ea62311669fe639a849713f57
Unkn0wn-Human <75699858+Unkn0wn-Human@users.noreply.github.com>
2021-12-02 00:50:12 -0500

    Testing for Module 5

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,1 +1,24 @@
+""" 
+    Title: mongodb_test.py
+    Author: Isaac Jakubo
+    Date: 2 December 2021
+    Description: Test program for connecting to a 
+                 MongoDB Atlas cluster
+"""
 
+#Importing Required modules
+from pymongo import MongoClient
+
+#Creating variable containing the MongoDB connection string
+url = "mongodb+srv://admin:admin@cluster0.bh3s1.mongodb.net/test"
+
+#Creating variable to connect to MongoDB cluster
+client = MongoClient(url)
+
+#Creating variable to for connecting to the Pytech database
+db = client.pytech
+
+#Printing the selected collections from the database and then exiting 
+print("\n -- Pytech COllection List -- ")
+print(db.list_collection_names())
+input("\n   End of program, press any key to exit...")
